bb7c48a2baa1a3a66b666d58dff7ce5f250b2627
POPFD <22840694+POPFD@users.noreply.github.com>
2022-02-20 17:14:03 +0000

    Amended run script to use same window rather than spawn new.

--- run-qemu.sh
+++ run-qemu.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-uefi-run -b /usr/share/OVMF/OVMF_CODE.fd -q /usr/bin/qemu-system-x86_64 build/hypervisor.efi
+# To exit do Ctrl-A X
+uefi-run -b /usr/share/OVMF/OVMF_CODE.fd -q /usr/bin/qemu-system-x86_64 build/hypervisor.efi -- -nographic
