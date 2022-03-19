43018bc210917632a01b0a750eb7dc3d6d695793
POPFD <22840694+POPFD@users.noreply.github.com>
2022-03-19 15:28:50 +0000

    Added loaded imagebase to initialisation.

--- hypervisor/start.c
+++ hypervisor/start.c
@@ -13,9 +13,14 @@ BOOLEAN _DYNAMIC = TRUE;
  
 EFI_STATUS
 EFIAPI
-efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
+efi_main (EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *system_table)
 {
-  InitializeLib(ImageHandle, SystemTable);
+  InitializeLib(image_handle, system_table);
+
+  EFI_LOADED_IMAGE *loaded_image = NULL;
+  uefi_call_wrapper(system_table->BootServices->HandleProtocol, 3,
+                    image_handle, &gEfiLoadedImageProtocolGuid, &loaded_image);
+  debug_print(L"EFI image loaded at: 0x%lX\n", loaded_image->ImageBase);
 
 //#define DEBUG_IDA
 #ifdef DEBUG_IDA
@@ -24,13 +29,11 @@ efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
   while (!wait_debug) {}
 #endif
   
-  debug_print(L"Hello world!\n");
-
   /* Initialise all of the required modules and set up the parameters
    * required for the VMM to start. */
   struct vmm_init_params vmm_params = {};
 
-  efi_plat_init(SystemTable);
+  efi_plat_init(system_table);
   pmem_init();
   vmem_init(&vmm_params.guest_cr3, &vmm_params.host_cr3);
   idt_init(&vmm_params.guest_idtr, &vmm_params.host_idtr);
