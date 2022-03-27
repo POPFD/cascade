0a22182208824ab3ddac3b6b2c62f40ce73c8b6c
POPFD <22840694+POPFD@users.noreply.github.com>
2022-03-27 21:18:13 +0100

    DEBUG: Set to 1 vCPU in script to allow for easier VMX debugging during dev.

--- run-qemu.sh
+++ run-qemu.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # To exit do Ctrl-A X
-./uefi-run/target/debug/uefi-run -b /usr/share/OVMF/OVMF_CODE.fd -q /usr/bin/qemu-system-x86_64 build/hypervisor.efi -- -nographic -enable-kvm -cpu host -smp 2
+./uefi-run/target/debug/uefi-run -b /usr/share/OVMF/OVMF_CODE.fd -q /usr/bin/qemu-system-x86_64 build/hypervisor.efi -- -nographic -enable-kvm -cpu host -smp 1
