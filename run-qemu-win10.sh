aa1e7a57ed1d1f4b62ade4a53b5ea813a1e5c64b
POPFD <22840694+POPFD@users.noreply.github.com>
2022-05-07 22:15:57 +0100

    Amended win10 script to load based off a snapshot...
    ... this prevents us from having to ensure a clean shutdown when
    closing the virtualmachine in qemu.

--- run-qemu-win10.sh
+++ run-qemu-win10.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+TARGET_IMG=/vm/win10-uefi-dev.qcow2
+TARGET_IMG_SNAP=$TARGET_IMG.snap
+
+# Create a snapshot of the win10 image we want to use.
+rm -f $TARGET_IMG_SNAP
+qemu-img create -f qcow2 -b $TARGET_IMG $TARGET_IMG_SNAP
+
 # To exit do Ctrl-A X
 ./uefi-run/target/debug/uefi-run -b /usr/share/OVMF/OVMF_CODE.fd -q /usr/bin/qemu-system-x86_64 build/hypervisor.efi -- \
-    -display gtk -enable-kvm -serial stdio -cpu host -smp 1 -drive file=/vm/win10-uefi-dev.qcow2,media=disk,if=ide,cache=off,index=1 -m 4G
+    -display gtk -enable-kvm -serial stdio -cpu host -smp 1 -drive file=$TARGET_IMG_SNAP,media=disk,if=ide,cache=off,index=1 -m 4G
