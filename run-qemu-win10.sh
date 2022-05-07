4a52728d5d7638fe37e81b6abf5c0ccd7dc7f12b
POPFD <22840694+POPFD@users.noreply.github.com>
2022-05-07 18:16:01 +0100

    Better serial printing for win10 script.

--- run-qemu-win10.sh
+++ run-qemu-win10.sh
@@ -2,4 +2,4 @@
 
 # To exit do Ctrl-A X
 ./uefi-run/target/debug/uefi-run -b /usr/share/OVMF/OVMF_CODE.fd -q /usr/bin/qemu-system-x86_64 build/hypervisor.efi -- \
-    -display gtk -enable-kvm -cpu host -smp 1 -drive file=/vm/win10-uefi-dev.qcow2,media=disk,if=ide,cache=off,index=1 -m 4G
+    -display gtk -enable-kvm -serial stdio -cpu host -smp 1 -drive file=/vm/win10-uefi-dev.qcow2,media=disk,if=ide,cache=off,index=1 -m 4G
