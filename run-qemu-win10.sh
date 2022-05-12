08cd00df72c47588a43880f6496b6f94056411a5
POPFD <22840694+POPFD@users.noreply.github.com>
2022-05-12 19:36:25 +0100

    Move included submodules to specific submodule directory.
    This is to ease confusion to what is cascade code and what isn't.

--- run-qemu-win10.sh
+++ run-qemu-win10.sh
@@ -8,5 +8,5 @@ rm -f $TARGET_IMG_SNAP
 qemu-img create -f qcow2 -b $TARGET_IMG $TARGET_IMG_SNAP
 
 # To exit do Ctrl-A X
-./uefi-run/target/debug/uefi-run -b /usr/share/OVMF/OVMF_CODE.fd -q /usr/bin/qemu-system-x86_64 build/hypervisor.efi -- \
+./submodules/uefi-run/target/debug/uefi-run -b /usr/share/OVMF/OVMF_CODE.fd -q /usr/bin/qemu-system-x86_64 build/hypervisor.efi -- \
     -display gtk -enable-kvm -serial stdio -cpu host -smp 1 -drive file=$TARGET_IMG_SNAP,media=disk,if=ide,cache=off,index=1 -m 4G
