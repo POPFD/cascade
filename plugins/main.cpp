d74cb2cc143f4707a8bba7648e2de8a7d7a3d6d8
POPFD <22840694+POPFD@users.noreply.github.com>
2022-06-08 16:52:18 +0100

    Adjusted plugin loader to take parameter for plugin path

--- plugins/main.cpp
+++ plugins/main.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 #include "hypervisor.hpp"
 
-int main()
+int main(int argc, char **argv)
 {
-    hypervisor hv;
+    if (argc != 2) {
+        std::cout << "Invalid number of arguments, please plugin path.\n";
+        return -EINVAL;
+    }
 
+    hypervisor hv;
     if (!hv.check_presence()) {
         std::cout << "Cascade not present!\n";
     } else {
         std::cout << "Cascade present!\n";
 
-        if (!hv.load_plugin("hello-world.dll")) {
+        if (!hv.load_plugin(argv[1])) {
             std::cout << "Unable to load plugin.\n";
         } else {
             std::cout << "Plugin loaded.\n";
