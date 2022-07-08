1950644829c71e21dcda3b7d1d7ab4bdd6215417
POPFD <22840694+POPFD@users.noreply.github.com>
2022-07-08 20:03:47 +0100

    Created script for starting a win10 VM with hyperv/hvci enabled.

---
+++ scripts/run-qemu-hvci-win10.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+TARGET_IMG=/vm/hvci-win10-uefi-dev.qcow2
+TARGET_IMG_SNAP=$TARGET_IMG.snap
+
+# Create a snapshot of the win10 image we want to use.
+rm -f $TARGET_IMG_SNAP
+qemu-img create -f qcow2 -b $TARGET_IMG $TARGET_IMG_SNAP
+
+# To exit do Ctrl-A X
+./submodules/uefi-run/target/debug/uefi-run -b /usr/share/OVMF/OVMF_CODE.fd -q /usr/bin/qemu-system-x86_64 build/hypervisor.efi -- \
+    -display gtk -enable-kvm -serial stdio -cpu host -smp 4 -m 8G \
+    -drive file=$TARGET_IMG_SNAP,media=disk,if=ide,cache=off,index=1 \
+    -drive file=fat:rw:./build/
