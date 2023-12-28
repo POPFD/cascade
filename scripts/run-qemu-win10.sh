371eb4eb4a2fc8d4b6b1eb65b3ea5a62ee584a40
POPFD <22840694+POPFD@users.noreply.github.com>
2023-12-28 15:32:38 +0000

    Update uefi-run submodule and adjust scripts
    To simplify the scripts, let's go back to not having to pass
    in an argument for the UEFI image. Just hard code it.
    
    Also as we're updating uefi-run we can now pass in extra
    binaries, let's pass in the hypervisor.a library just to
    test that.

--- scripts/run-qemu-win10.sh
+++ scripts/run-qemu-win10.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-TARGET_UEFI_IMAGE=$1
 TARGET_IMG=/vm/win10-uefi-dev.qcow2
 TARGET_IMG_SNAP=$TARGET_IMG.snap
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Create a snapshot of the win10 image we want to use.
 rm -f $TARGET_IMG_SNAP
 qemu-img create -f qcow2 -F qcow2 -b $TARGET_IMG $TARGET_IMG_SNAP
 
 # To exit do Ctrl-A X
-$SCRIPT_DIR/../submodules/uefi-run/target/debug/uefi-run -b /usr/share/OVMF/OVMF_CODE.fd -q /usr/local/bin/qemu-system-x86_64 $TARGET_UEFI_IMAGE -- \
+./submodules/uefi-run/target/debug/uefi-run -b /usr/share/OVMF/OVMF_CODE.fd -q /usr/local/bin/qemu-system-x86_64 -d \
+    build/uefi.efi -f build/hypervisor.a -- \
     -display gtk -enable-kvm -serial stdio -cpu host -smp 4 -m 8G \
     -drive file=$TARGET_IMG_SNAP,media=disk,if=ide,cache=off,index=1 \
     -drive file=fat:rw:./build/
