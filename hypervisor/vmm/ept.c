9dbec15161c7a09942fee2f32b0166f71943e643
POPFD <22840694+POPFD@users.noreply.github.com>
2023-12-30 23:57:37 +0000

    Fix broken MTRR code on newer processors
    MTRR has been broken since the beginning, we were not taking
    into account the fixed MTRR ranges but also we assumed that
    by default any out of range areas would be WB.
    
    This is NOT true, this has caused me so much pain over the
    past few weeks trying to get this project to boot on a 13th
    gen Intel.
    
    Now we properly deal with the MTRRs.

--- hypervisor/vmm/ept.c
+++ hypervisor/vmm/ept.c
@@ -11,6 +11,7 @@
 
 struct mtrr_data {
     bool valid;
+    bool is_fixed;
     uint8_t type;
     size_t phys_base_min;
     size_t phys_base_max;
@@ -32,6 +33,8 @@ struct ept_ctx {
     /* List of MTRR data, not all may be valid as each processor arch
      * can vary depending on the amount of MTRRs implemented. */
     struct mtrr_data mtrr[IA32_MTRR_COUNT];
+    size_t mtrr_count;
+    uint8_t def_mem_type;
 };
 
 struct ept_split_page {
@@ -42,27 +45,102 @@ struct ept_split_page {
     ept_pde_2mb *pml2e_ref;
 };
 
-static void gather_mtrr_list(struct ept_ctx *ctx)
+static void gather_fixed_mtrr(struct ept_ctx *ctx)
 {
-    ia32_mtrrcap_register caps;
+    struct fixed_mtrr_info {
+        uint64_t msr_id;
+        uintptr_t base_address;
+        size_t managed_size;
+    };
+
+    typedef union {
+        struct {
+            uint8_t types[8];
+        } u;
+        uint64_t flags;
+    } ia32_mtrr_fixed_range_msr;
+
+    static const struct fixed_mtrr_info FIXED_INFO[] = {
+        { IA32_MTRR_FIX64K_00000, 0x0, 0x10000, },
+        { IA32_MTRR_FIX16K_80000, 0x80000, 0x4000, },
+        { IA32_MTRR_FIX16K_A0000, 0xA0000, 0x4000, },
+        { IA32_MTRR_FIX4K_C0000,  0xC0000, 0x1000, },
+        { IA32_MTRR_FIX4K_C8000,  0xC8000, 0x1000, },
+        { IA32_MTRR_FIX4K_D0000,  0xD0000, 0x1000, },
+        { IA32_MTRR_FIX4K_D8000,  0xD8000, 0x1000, },
+        { IA32_MTRR_FIX4K_E0000,  0xE0000, 0x1000, },
+        { IA32_MTRR_FIX4K_E8000,  0xE8000, 0x1000, },
+        { IA32_MTRR_FIX4K_F0000,  0xF0000, 0x1000, },
+        { IA32_MTRR_FIX4K_F8000,  0xF8000, 0x1000, },
+    };
+
+    ia32_mtrrcap_register caps = { .flags = rdmsr(IA32_MTRRCAP) };
+    ia32_mtrr_def_type_register def_type = { .flags = rdmsr(IA32_MTRR_DEF_TYPE) };
+
+    /* Store the default memory type, for all regions not covered by a MTRR. */
+    ctx->def_mem_type = def_type.default_memory_type;
+
+    if (!caps.fixed_range_registers_supported || !def_type.fixed_range_mtrr_enable)
+        return;
+
+    struct mtrr_data *last_valid = NULL;
+
+    for (size_t i = 0; i < ARRAY_SIZE(FIXED_INFO); i++) {
+        const struct fixed_mtrr_info *curr_range = &FIXED_INFO[i];
+
+        ia32_mtrr_fixed_range_msr fixed_range = {.flags = rdmsr(curr_range->msr_id) };
+
+        for (size_t j = 0; j < ARRAY_SIZE(fixed_range.u.types); j++) {
+            uint8_t mem_type = fixed_range.u.types[j];
+            uint64_t range_begin = curr_range->base_address + (curr_range->managed_size * j);
+            uint64_t range_end = range_begin + curr_range->managed_size;
+
+            /*
+             * Check to see if we can combine it with previous.
+             * For this to be true, it must be of the same memory type
+             * and also be contiguous.
+             *
+             * This will make it easier & quicker for when we do searching.
+             */
+            if (last_valid && (last_valid->type == mem_type) &&
+                (last_valid->phys_base_max == range_begin)) {
+
+                last_valid->phys_base_max += curr_range->managed_size;
+                // DEBUG_PRINT("Extended last fixed entry to phys_based_min=0x%lX phys_base_max=0x%lX",
+                //             last_valid->phys_base_min, last_valid->phys_base_max);
+            } else {
+                struct mtrr_data *new_entry = &ctx->mtrr[ctx->mtrr_count];
+                new_entry->valid = true;
+                new_entry->is_fixed = true;
+                new_entry->phys_base_min = range_begin;
+                new_entry->phys_base_max = range_end;
+                new_entry->type = mem_type;
+
+                // DEBUG_PRINT("Adding fixed entry phys_base_min=0x%lX phys_base_max=0x%lX type=0x%lX",
+                //             new_entry->phys_base_min, new_entry->phys_base_max, new_entry->type);
+
+                last_valid = new_entry;
+                ctx->mtrr_count++;
+            }
+        }
+    }
+}
+
+static void gather_variable_mtrr(struct ept_ctx *ctx)
+{
+    ia32_mtrrcap_register caps = { .flags = rdmsr(IA32_MTRRCAP) };
     ia32_mtrr_physbase_register base;
     ia32_mtrr_physmask_register mask;
 
-    /* Query the capabilities to determine the number of registers. */
-    caps.flags = rdmsr(IA32_MTRRCAP);
+
+    struct mtrr_data *last_valid = NULL;
+
     for (size_t i = 0; i < caps.variable_range_registers_count; i++) {
-        struct mtrr_data *mtrr = &ctx->mtrr[i];
-
-        base.flags = rdmsr(IA32_MTRR_PHYSBASE0 + i * 2);
-        mask.flags = rdmsr((IA32_MTRR_PHYSBASE0 + 1) + i * 2);
-
-        /* Store the mtrr type and whether valid. */
-        mtrr->type = base.type;
-        mtrr->valid = (bool)mask.valid;
+        base.flags = rdmsr(IA32_MTRR_PHYSBASE0 + (i * 2));
+        mask.flags = rdmsr(IA32_MTRR_PHYSMASK0 + (i * 2));
 
         /* If the mtrr is valid, calculate the min and maximum ranges. */
-        if (mtrr->valid) {
-            mtrr->phys_base_min = base.physical_addres_base * PAGE_SIZE;
+        if (mask.valid) {
 
             /*
              * __builtin_ffsll returns 1 + the index of the least significate 1-bit of x.
@@ -71,36 +149,99 @@ static void gather_mtrr_list(struct ept_ctx *ctx)
             long long bit_idx = __builtin_ffsll(mask.physical_addres_mask) - 1;
             size_t size_in_pages = (1ull << bit_idx);
 
-            mtrr->phys_base_max = mtrr->phys_base_min + (size_in_pages * PAGE_SIZE);
-        }
+            uint8_t mem_type = base.type;
+            uint64_t range_begin = base.physical_addres_base * PAGE_SIZE;
+            uint64_t range_size = size_in_pages * PAGE_SIZE;
+            uint64_t range_end = range_begin + range_size;
 
-        DEBUG_PRINT("Valid %d type %d base_min %lX base_max %lX",
-                  mtrr->valid, mtrr->type, mtrr->phys_base_min, mtrr->phys_base_max);
+            if (last_valid && (last_valid->type == mem_type) &&
+                (last_valid->phys_base_max == range_begin)) {
+
+                last_valid->phys_base_max += range_size;
+                // DEBUG_PRINT("Extended last variable entry to phys_base_min=0x%lX phys_base_max=0x%lX",
+                //             last_valid->phys_base_min, last_valid->phys_base_max);
+            } else {
+                struct mtrr_data *new_entry = &ctx->mtrr[ctx->mtrr_count];
+                new_entry->valid = true;
+                new_entry->is_fixed = false;
+                new_entry->phys_base_min = range_begin;
+                new_entry->phys_base_max = range_end;
+                new_entry->type = mem_type;
+
+                // DEBUG_PRINT("Adding variable phys_base_min=0x%lX phys_base_max=0x%lX type=0x%lX",
+                //             new_entry->phys_base_min, new_entry->phys_base_max, new_entry->type);
+
+                last_valid = new_entry;
+                ctx->mtrr_count++;
+            }
+        }
     }
 }
 
-static uint32_t adjust_memory_type(struct ept_ctx *ctx, uintptr_t addr, uint32_t type)
+static void gather_mtrr_list(struct ept_ctx *ctx)
 {
-    /* Check to see if the specified address falls within
-     * any of the MTRR ranges, if so we need to adjust the
-     * effective memory type. */
-    uint32_t new_type = type;
-    for (int i = 0; i < IA32_MTRR_COUNT; i++) {
+    gather_fixed_mtrr(ctx);
+    gather_variable_mtrr(ctx);
 
-        /* Not a valid entry, skip. */
-        if (!ctx->mtrr[i].valid)
+    /* Let's print our memory type information. */
+    DEBUG_PRINT("Default memory type=%d", ctx->def_mem_type);
+    for (size_t i = 0; i < IA32_MTRR_COUNT; i++) {
+        const struct mtrr_data *curr_mtrr = &ctx->mtrr[i];
+
+        if (curr_mtrr->valid)
+            DEBUG_PRINT("Range begin=0x%016llX end=0x%016llX type=%d fixed=%d",
+                        curr_mtrr->phys_base_min, curr_mtrr->phys_base_max,
+                        curr_mtrr->type, curr_mtrr->is_fixed);
+    }
+}
+
+static uint32_t calc_mem_type(struct ept_ctx *ctx, uintptr_t phys_begin, size_t phys_size)
+{
+    /*
+     * Iterate all of the MTRRs we have defined, and check to see if they match any of
+     * the range which we have specified, if so we then use that MTRR's value.
+     */
+    uint32_t mem_type = MEMORY_TYPE_INVALID;
+
+    for (size_t i = 0; i < IA32_MTRR_COUNT; i++) {
+        const struct mtrr_data *curr_mtrr = &ctx->mtrr[i];
+
+        /* Filter out invalid/empty entries. */
+        if (!curr_mtrr->valid)
             continue;
 
-        /* Check to see if boundary falls anywhere within range. */
-        if (((addr + (MiB(2) - 1)) >= ctx->mtrr[i].phys_base_min) &&
-            (addr <= ctx->mtrr[i].phys_base_max)) {
-            
-            new_type = ctx->mtrr[i].type;
+        /* If out of range, let's skip. */
+        if ((phys_begin < curr_mtrr->phys_base_min) ||
+            (phys_begin >= curr_mtrr->phys_base_max))
+            continue;
+
+        /* If the range of our phys region is larger than defined in the MTRR throw an error. */
+        if ((phys_begin + phys_size - 1) >= curr_mtrr->phys_base_max)
+            return mem_type;
+
+        /* Fixed MTRRs take precedence over all others. */
+        if (curr_mtrr->is_fixed)
+            return curr_mtrr->type;
+
+        /* Uncacheable takes next precedence. */
+        if (curr_mtrr->type == MEMORY_TYPE_UC)
+            return curr_mtrr->type;
+
+        /* Writethrough always takes precedence over writeback memory. */
+        if (((curr_mtrr->type == MEMORY_TYPE_WT) && (mem_type == MEMORY_TYPE_WB)) ||
+            ((curr_mtrr->type == MEMORY_TYPE_WB) && (mem_type == MEMORY_TYPE_WT))) {
+            mem_type = MEMORY_TYPE_WT;
         }
+
+        /* Anything else, just set to last matched. */
+        mem_type = curr_mtrr->type;
     }
 
-    /* If not in MTRR list, return desired. */
-    return new_type;
+    /* If we didn't find the value in the MTRR list then just use the default. */
+    if (mem_type == MEMORY_TYPE_INVALID)
+        mem_type = ctx->def_mem_type;
+
+    return mem_type;
 }
 
 static void ept_split_large_page(struct ept_ctx *ctx, uintptr_t phys_addr)
@@ -124,10 +265,12 @@ static void ept_split_large_page(struct ept_ctx *ctx, uintptr_t phys_addr)
         .read_access = true,
         .write_access = true,
         .execute_access = true,
-        .memory_type = target_pml2e->memory_type,
         .ignore_pat = target_pml2e->ignore_pat,
         .suppress_ve = target_pml2e->suppress_ve
     };
+
+    die_on(temp_pte.memory_type == MEMORY_TYPE_INVALID,
+           "Memory type for 0x%lX is invalid even with splitting.", phys_addr);
 
     /* Calculate the physical address of the original PML2 entry.
      * and the page frame number, we will use this as base. */
@@ -140,8 +283,12 @@ static void ept_split_large_page(struct ept_ctx *ctx, uintptr_t phys_addr)
      * As we have calculated the original PFN for the PML2E we can
      * we can just add one page for each entry. */
     for (int i = 0; i < ENTRIES_PER_TABLE; i++) {
+        size_t curr_pfn = base_pfn + i;
+        uintptr_t curr_phys = curr_pfn * PAGE_SIZE;
+
         new_split->pml1[i] = temp_pte;
-        new_split->pml1[i].page_frame_number = base_pfn + i;
+        new_split->pml1[i].memory_type = calc_mem_type(ctx, curr_phys, PAGE_SIZE);
+        new_split->pml1[i].page_frame_number = curr_pfn;
     }
 
     /* Now create a new PML2E entry to replace the old one. */
@@ -211,8 +358,21 @@ struct ept_ctx *ept_init(void)
             ctx->pml2[i][j].page_frame_number = (i * ENTRIES_PER_TABLE) + j;
 
             uintptr_t phys_addr = ctx->pml2[i][j].page_frame_number * MiB(2);
-            uint32_t mem_type = adjust_memory_type(ctx, phys_addr, MEMORY_TYPE_WB);
-            ctx->pml2[i][j].memory_type = mem_type;
+
+            /* Calculate the memory type for this entry. */
+            uint32_t mem_type = calc_mem_type(ctx, phys_addr, MiB(2));
+
+            /*
+             * If the memory type is invalid (too large but semi in a 2MiB page)
+             * Then let's split the it into smaller PML1E entries and then re-calc
+             * the memory type individually.
+             */
+            if (mem_type != MEMORY_TYPE_INVALID) {
+                ctx->pml2[i][j].memory_type = mem_type;
+            } else {
+                DEBUG_PRINT("Memory type for 0x%lX is invalid, splitting.", phys_addr);
+                ept_split_large_page(ctx, phys_addr);
+            }
         }
     }
 
